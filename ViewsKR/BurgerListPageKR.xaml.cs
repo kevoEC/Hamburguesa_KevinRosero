270b588db04a27c552eeb85bc02e3b42c53b2ddc
Kevin Rosero <kevin.rosero@udla.edu.ec>
2023-01-16 00:35:31 -0500

    Se agrega el auxiliar en el ListPage para poder enviarlo, ademas se implementa completamente la funcionalidad de borrar

--- ViewsKR/BurgerListPageKR.xaml.cs
+++ ViewsKR/BurgerListPageKR.xaml.cs
@@ -5,11 +5,13 @@ namespace Hamburguesa_KevinRosero.ViewsKR;
 
 public partial class BurgerListPageKR : ContentPage
 {
-	public BurgerListPageKR()
+    BurgerKR selected;
+    public BurgerListPageKR()
 	{
 		InitializeComponent();
-        List<BurgerKR> burger = App.BurgerRepoKR.GetAllBurgers();
-        burgerList.ItemsSource = burger;
+        //List<BurgerKR> burger = App.BurgerRepoKR.GetAllBurgers();
+        //burgerList.ItemsSource = burger;
+        burgerListKR.ItemsSource = UpdateList();
     }
 
     async void OnItemAdded(object sender, EventArgs e)
@@ -19,12 +21,24 @@ public partial class BurgerListPageKR : ContentPage
 
     private void ActualizarDatos(object sender, EventArgs e)
     {
-        List<BurgerKR> burger = App.BurgerRepoKR.GetAllBurgers();
-        burgerList.ItemsSource = burger;
+        //List<BurgerKR> burger = App.BurgerRepoKR.GetAllBurgers();
+        burgerListKR.ItemsSource = App.BurgerRepoKR.GetAllBurgers();
+        //burgerListEM.ItemTapped += OnBurgerSelected;
     }
 
-    async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    private async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        await Shell.Current.GoToAsync(nameof(BurgerItemPageKR));
+        selected = e.CurrentSelection[0] as BurgerKR;
+        await Navigation.PushAsync(new BurgerItemPageKR
+        {
+            auxKR = selected,
+            BindingContext = selected,
+        });
+    }
+
+    private static List<BurgerKR> UpdateList()
+    {
+        List<BurgerKR> burger = App.BurgerRepoKR.GetAllBurgers();
+        return burger;
     }
 }
